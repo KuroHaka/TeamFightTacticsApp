412436ca82beb08006814a0b7899cc257ba61719
zhensk27 <ceschen27@gmail.com>
2019-07-30 04:27:02 +0200

    iniciado montaje del menú principal, problema de compatibilidad de imágen grave encontrado, he de redimensionar varios componentes
    Signed-off-by: zhensk27 <ceschen27@gmail.com>

--- app/src/main/java/com/GameCode/teamfighttacticsapp/MainActivity.java
+++ app/src/main/java/com/GameCode/teamfighttacticsapp/MainActivity.java
@@ -2,7 +2,9 @@ package com.GameCode.teamfighttacticsapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //fuente
+        String carpetaFuente = "fuente/TFTfont.otf";
+        TextView txt1 = findViewById(R.id.txt1);
+        Typeface fuente = Typeface.createFromAsset(getAssets(), carpetaFuente);
+
+        txt1.setTypeface(fuente);
     }
 }
