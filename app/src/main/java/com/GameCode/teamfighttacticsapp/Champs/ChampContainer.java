a5e762bd6e6f7b1576874fbac5dbe216ce951b0d
Dragris <dragris1234@gmail.com>
2019-08-02 18:00:25 +0200

    Añadidos todos los campeones sin precio ni imágenes.
    Signed-off-by: Dragris <dragris1234@gmail.com>

--- app/src/main/java/com/GameCode/teamfighttacticsapp/Champs/ChampContainer.java
+++ app/src/main/java/com/GameCode/teamfighttacticsapp/Champs/ChampContainer.java
@@ -50,8 +50,30 @@ public class ChampContainer {
         Champs kha = new Champs(0, "Kha'Zix", "Saborea su Miedo", 0, 1, new Origen[]{origen.Vacio()}, new Clase[]{clase.Asesino()}); champList.add(kha);
         Champs kindred = new Champs(0, "Kindred", "Alivio de Cordero", 0, 4, new Origen[]{origen.Fantasma()}, new Clase[]{clase.Ranger()}); champList.add(kindred);
         Champs leona = new Champs(0, "Leona", "Llamarada Solar", 0, 4, new Origen[]{origen.Noble()}, new Clase[]{clase.Guardian()}); champList.add(leona);
-        Champs lissandra = new Champs (0, "Lissandra", "Tumba congelada", 0, 2, new Origen[]{origen.Glacial()}, new Clase[]{clase.Elementalista()}); champList.add(lissandra);
-
+        Champs lissandra = new Champs(0, "Lissandra", "Tumba congelada", 0, 2, new Origen[]{origen.Glacial()}, new Clase[]{clase.Elementalista()}); champList.add(lissandra);
+        Champs lucian = new Champs(0, "Lucian", "Persecución Implacable", 0, 2, new Origen[]{origen.Noble()}, new Clase[]{clase.Pistolero()}); champList.add(lucian);
+        Champs lulu = new Champs(0, "Lulu", "Enormamiento", 0, 2, new Origen[]{origen.Yordle()}, new Clase[]{clase.Hechicero()}); champList.add(lulu);
+        Champs miss = new Champs(0, "Miss Fortune", "Balacera", 0, 5, new Origen[]{origen.Pirata()}, new Clase[]{clase.Pistolero()}); champList.add(miss);
+        Champs mordekaiser = new Champs(0, "Mordekaiser", "Aniquilación", 0, 1, new Origen[]{origen.Fantasma()}, new Clase[]{clase.Caballero()}); champList.add(mordekaiser);
+        Champs morgana = new Champs(0, "Morgana", "Grilletes del Alma", 0, 3, new Origen[]{origen.Demonio()}, new Clase[] {clase.Hechicero()}); champList.add(morgana);
+        Champs nidaleee = new Champs(0, "Nidaleee", "Arrebato Primitivo", 0, 1, new Origen[]{origen.Salvaje()}, new Clase[]{clase.Cambiaformas()}); champList.add(nidaleee);
+        Champs poppy = new Champs(0, "Poppy", "Veredicto de la Guardiana", 0, 3, new Origen[]{origen.Yordle()}, new Clase[]{clase.Caballero()}); champList.add(poppy);
+        Champs pyke = new Champs(0, "Pyke", "Corriente Fantasma", 0, 2, new Origen[]{origen.Pirata()}, new Clase[]{clase.Asesino()}); champList.add(pyke);
+        Champs rek = new Champs(0, "Rek'Sai", "Excavar", 0, 2, new Origen[]{origen.Vacio()}, new Clase[]{clase.Luchador()}); champList.add(rek);
+        Champs rengar = new Champs(0, "Rengar", "Fiereza", 0, 3, new Origen[]{origen.Salvaje()}, new Clase[]{clase.Asesino()}); champList.add(rengar);
+        Champs sejuani = new Champs(0, "Sejuani", "Prisión Glacial", 0, 4, new Origen[]{origen.Glacial()}, new Clase[]{clase.Caballero()}); champList.add(sejuani);
+        Champs shen = new Champs(0, "Shen", "Refugio del Espíritu", 0, 2, new Origen[]{origen.Ninja()}, new Clase[]{clase.Espadachin()}); champList.add(shen);
+        Champs shyvana = new Champs(0, "Shyvana", "Descendiente del Dragón", 0, 3, new Origen[]{origen.Dragon()}, new Clase[]{clase.Cambiaformas()}); champList.add(shyvana);
+        Champs swain = new Champs(0, "Swain", "Llamarada Infernal", 0, 5, new Origen[]{origen.Demonio(), origen.Imperial()}, new Clase[]{clase.Cambiaformas()}); champList.add(swain);
+        Champs tristana = new Champs(0, "Tristana", "Carga Explosiva", 0, 1, new Origen[]{origen.Yordle()}, new Clase[]{clase.Pistolero()}); champList.add(tristana);
+        Champs twisted = new Champs(0, "Twisted Fate", "Escoge una Carta", 0, 2, new Origen[]{origen.Pirata()}, new Clase[]{clase.Hechicero()}); champList.add(twisted);
+        Champs varus = new Champs(0, "Varus", "Flecha Penetrante", 0, 2, new Origen[]{origen.Demonio()}, new Clase[]{clase.Ranger()}); champList.add(varus);
+        Champs vayne = new Champs (0, "Vayne", "Proyectiles de Plata", 0, 1, new Origen[]{origen.Noble()}, new Clase[]{clase.Ranger()}); champList.add(vayne);
+        Champs veigar = new Champs(0, "Veigar", "Estallido Primordial", 0, 3, new Origen[]{origen.Yordle()}, new Clase[]{clase.Hechicero()}); champList.add(veigar);
+        Champs volibear = new Champs(0, "Volibear", "Garras de Trueno", 0, 3, new Origen[]{origen.Glacial()}, new Clase[]{clase.Luchador()}); champList.add(volibear);
+        Champs warwick = new Champs(0, "Warwick", "Opresión Infinita", 0, 1, new Origen[]{origen.Salvaje()}, new Clase[]{clase.Luchador()}); champList.add(warwick);
+        Champs yasuo = new Champs(0, "Yasuo", "Tempestad de Acero", 0, 5, new Origen[]{origen.Exiliado()}, new Clase[]{clase.Espadachin()}); champList.add(yasuo);
+        Champs zed = new Champs(0, "Zed", "Navaja Shuriken", 0, 2, new Origen[]{origen.Ninja()}, new Clase[]{clase.Asesino()}); champList.add(zed);
     }
 
     public ArrayList<Champs> getByName(String searching){
